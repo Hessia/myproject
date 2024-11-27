74ccf9d6c2417847f99245a20895ee79fa172ba2
Elif <hessia2006@gmail.com>
2024-11-27 16:26:58 +0000

    Add files via upload

---
+++ myproject_user.py
@@ -0,0 +1,27 @@
+# i create separate file for users so it wont look messy
+import hashlib
+def hash_password(password):
+    """Hash a password for secure storage."""
+    return hashlib.sha256(password.encode()).hexdigest()
+
+def register_user(username, password, file_path="users.txt"):
+    """Register a new user by saving their credentials."""
+    with open(file_path, "a") as file:
+        file.write(f"{username}, {hash_password(password)}\n")
+        print("User registered succesfully!")
+
+def login_user(username, password, file_path="users.txt"):
+    """Check if the user exists and the password matches."""
+    hashed_password = hash_password(password)
+    try:
+        with open(file_path, "r") as file:
+            for line in file:
+                stored_username, stored_password = line.strip().split(",")
+                if username == stored_username and hashed_password == stored_password:
+                    print("Login succesful!")
+                    return True
+    except FileNotFoundError:
+        print("User file not found.")
+    print("Invalid password or username.")
+    return False
+
